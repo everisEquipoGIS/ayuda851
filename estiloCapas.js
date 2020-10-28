feb9bc8466d34f532fbd279a9d47c682c8b96cd6
lreinape <lreinape@everis.com>
2020-10-28 15:02:18 +0100

    Añadir fichero para estilo de capas y eliminar fichero no necesario

---
+++ estiloCapas.js
@@ -0,0 +1,29 @@
+const capas = map.getLayers();
+const textoProvincias = capas.find(capa => capa.name === "TextoProvincias");
+
+
+let estiloTextoProvincia = new M.style.Point(
+	label: {
+		text: 'PROVINCIA', // Texto etiqueta. fijo|función|atributo
+		font: 'bold 16px Courier New', // Fuente de la etiqueta
+		scale: 0.9, // Escala de la etiqueta
+		offset: [10, 0], // Desplazamiento en píxeles
+		color: '#000', // Color del texto
+		// Halo del texto
+		stroke: {
+			color: '#FFF', // Color del halo
+			width: 2, // Grosor del halo
+			linedash: [5,5,20], // Patrón de distancias
+			linedashoffset: 1, // Offset de fase
+			linecap: 'square', // Estilo de final de linea. round|square|butt
+			linejoin: 'miter', // Estilo de conexión de segmentos. miter|round|bevel
+			miterlimit: 15 // Tamaño máximo segmento de conexión
+		},
+		rotate: false, // Rotación con dispositivo
+		rotation: 0.3, // Rotación de la etiqueta
+		align: M.style.align.CENTER, // Alineación horizontal. CENTER|LEFT|RIGHT
+		baseline: M.style.baseline.MIDDLE // Alineación vertical. TOP|BOTTOM|MIDDLE
+	}
+});
+
+textoProvincias.setStyle(estiloTextoProvincia);
