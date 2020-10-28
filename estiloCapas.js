d924f72433ea7ba181aaf2a36926ed675269f61d
lreinape <lreinape@everis.com>
2020-10-28 17:10:44 +0100

    prueba estilos

--- estiloCapas.js
+++ estiloCapas.js
@@ -3,12 +3,12 @@ const textoProvincias = capas.find(capa => capa.name === "ProvinciasTextos");
 
 console.log("Entra estilos capas");
 
-const estiloTextoProvincia = new M.style.Point(
+const estiloTextoProvincia = new M.style.Point({
 	fill: {  
       color: 'green',
       opacity: 0.8
     }
 	
-);
+});
 
 textoProvincias.setStyle(estiloTextoProvincia);
