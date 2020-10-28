4ef800576e2868e59843c5d4f9f334b91c4a5ef6
lreinape <lreinape@everis.com>
2020-10-28 19:55:44 +0100

    escala

--- estiloCapas.js
+++ estiloCapas.js
@@ -32,4 +32,4 @@ textoProvincias.setStyle(estiloTextoProvincia);
 
 
 /* Estableces escala minima a capa ProvinciaTexto */
-textoProvincias.getImpl().getOL3Layer().setMinResolution(15);
+textoProvincias.getImpl().getOL3Layer().setMinResolution(10);
