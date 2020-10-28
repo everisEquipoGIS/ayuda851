49ea8fe87130eb2ce493dbb4ccd67dff29aab0cc
lreinape <lreinape@everis.com>
2020-10-28 18:14:15 +0100

    estilo capas texto

--- estiloCapas.js
+++ estiloCapas.js
@@ -8,14 +8,14 @@ const estiloTextoProvincia = new M.style.Point({
 		text: function(feature) {
 		  return feature.getAttribute('PROVINCIA')
 		}, // Texto etiqueta. fijo|función|atributo
-		font: 'bold 16px Courier New', // Fuente de la etiqueta
+		font: 'bold 18px Courier New', // Fuente de la etiqueta
 		scale: 0.9, // Escala de la etiqueta
 		offset: [10, 0], // Desplazamiento en píxeles
 		color: '#000', // Color del texto
 		// Halo del texto
 		stroke: {
 			color: '#FFF', // Color del halo
-			width: 2, // Grosor del halo
+			width: 3, // Grosor del halo
 			linedash: [5,5,20], // Patrón de distancias
 			linedashoffset: 1, // Offset de fase
 			linecap: 'square', // Estilo de final de linea. round|square|butt
@@ -23,7 +23,7 @@ const estiloTextoProvincia = new M.style.Point({
 			miterlimit: 15 // Tamaño máximo segmento de conexión
 		},
 		rotate: false, // Rotación con dispositivo
-		rotation: 0.3, // Rotación de la etiqueta
+		rotation: 0, // Rotación de la etiqueta
 		align: M.style.align.CENTER, // Alineación horizontal. CENTER|LEFT|RIGHT
 		baseline: M.style.baseline.MIDDLE // Alineación vertical. TOP|BOTTOM|MIDDLE
 	}	
