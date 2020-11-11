c23772b7ef2401e3da33d3a41f7fc8aa6c2cf241
lreinape <lreinape@everis.com>
2020-11-11 09:52:02 +0100

    boton catastro

--- estiloCapas.js
+++ estiloCapas.js
@@ -48,10 +48,13 @@ capaFCC.getImpl().getOL3Layer().setMaxResolution(20);
 
 /* Cambiar color boton comunicacion catastro */
 var botonComuCatastro = document.getElementById("plg_comunicacion_catastro");
+// Incialmente poner en gris el boton
 botonComuCatastro.classList.add("desactivar_comunicacion_catastro");
 
 botonComuCatastro.click((e) => {
-	if(!botonComuCatastro.classList.contains("desactivar_comunicacion_catastro")){
+	if(botonComuCatastro.classList.contains("desactivar_comunicacion_catastro")){
+		botonComuCatastro.classList.remove("desactivar_comunicacion_catastro");
+	} else {
 		botonComuCatastro.classList.add("desactivar_comunicacion_catastro");
 	}	
 });	
